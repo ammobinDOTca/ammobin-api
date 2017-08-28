6873eb13d616fb70a006e724e14c255191e99fcd
Simon Ramsay <nexus-uw@users.noreply.github.com>
2017-08-27 21:18:13 -0700

    added frontierfirearms (#36)

--- test.js
+++ test.js
@@ -1,25 +1,3 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const ammotype = 'ammotype';
-const subtype = 'subtype';
+const ff = require('./frontierfirearms');
 
-axios.get(`http://www.cabelas.ca/checkproductvariantavailability/${ammotype}?specs=${subtype}`)
-  .then(r => {
-    const $ = cheerio.load(r.data)
-
-    const titles = [];
-    $('thead').find('th').each((index, row) => titles.push($(row).text()));
-
-    const items = [];
-    $('tbody').find('tr').each((index, row) => {
-      const result = [];
-      result.push($(row).find('a').prop('href'))
-      $(row).find('td').each((i, f) => {
-        result.push($(f).text().trim());
-      });
-      items.push(result);
-    })
-
-    return { items, titles };
-  })
-  .then(r => console.log(r))
+ff('centerfire').then(r => console.log(r))
