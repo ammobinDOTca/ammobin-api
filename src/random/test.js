2788e11393525a3eb4036de9e9a2ff6b8ef94b08
Simon R <nexus-uw@users.noreply.github.com>
2017-12-17 16:37:32 -0500

    added theshootingcenter.com

--- src/random/test.js
+++ src/random/test.js
@@ -1,4 +1,4 @@
-const test = require('../scrapes/barton')
+const test = require('../scrapes/shooting-edge')
 
-test('centerfire').then(f => console.log(f)).catch(e => console.error('ERROR', e))
+test('centerfire').then(f => console.log(f.length)).catch(e => console.error('ERROR', e))
 
