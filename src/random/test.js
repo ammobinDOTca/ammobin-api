8552ba9515c6c1546210a07fc5623f8859613b47
Simon Ramsay <nexus-uw@users.noreply.github.com>
2018-02-11 15:36:59 -0500

    added soley outdoors

--- src/random/test.js
+++ src/random/test.js
@@ -1,4 +1,4 @@
-const test = require('../scrapes/durhamoutdoors')
+const test = require('../scrapes/soley-outdoors')
 
-test('centerfire').then(f => console.log(f)).catch(e => console.error('ERROR', e.message))
+test('rimfire').then(f => console.log(f)).catch(e => console.error('ERROR', e.message))
 
---
+++ src/scrapes/soley-outdoors.js
@@ -0,0 +1,65 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const throat = require('throat');
+const helpers = require('../helpers');
+
+function work(type, page = 1) {
+  console.log(`loading solely outdoors ${page}`)
+  return axios.get(`https://www.solelyoutdoors.com/ammunition/${type}/page${page}.html`)
+    .then(r => {
+      let $ = cheerio.load(r.data)
+      const items = [];
+      $('.product').each((index, row) => {
+        const result = {};
+        const tha = $(row);
+
+
+        result.link = tha.find('.image-wrap a').prop('href');
+        result.img = tha.find('.image-wrap img').prop('src');
+        result.name = tha.find('.image-wrap a').prop('title').trim();
+        const priceTxt = tha.find('.info .left').text();
+        result.price = parseFloat(priceTxt.replace('C$', ''));
+        result.vendor = 'Soley Outdoors';
+        result.province = 'ON'
+
+        items.push(result);
+      })
+
+      if ($('.next.enabled').length) {
+        $ = null; // dont hold onto page for recursion
+        return work(type, page + 1)
+          .then(results => items.concat(results));
+      } else {
+        return items;
+      }
+
+    })
+}
+
+function solelyoutdoors(type) {
+  const throttle = throat(1);
+
+  switch (type) {
+    case 'rimfire':
+      return work('rimfire-ammo', 1)
+        .then(helpers.classifyRimfire);
+
+    case 'centerfire':
+      return Promise.all([
+        'centerfire-ammo',
+        'handgun-ammo',
+        'rifle-ammo'
+      ]
+        .map(t => throttle(() => work(t))))
+        .then(helpers.combineResults)
+        .then(helpers.classifyCenterfire);
+
+    case 'shotgun':
+      return work('shotgun-ammo', 1).then(helpers.classifyShotgun);
+
+    default:
+      return Promise.reject(new Error('unknown type: ' + type));
+  }
+}
+
+module.exports = solelyoutdoors;
