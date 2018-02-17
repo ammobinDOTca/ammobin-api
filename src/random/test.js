0fdc697e9096f80955b425e319168844b3424867
Simon R <nexus-uw@users.noreply.github.com>
2018-02-17 12:29:07 -0500

    added north pro sports (SK)

--- src/random/test.js
+++ src/random/test.js
@@ -1,4 +1,4 @@
-const test = require('../scrapes/soley-outdoors')
+const test = require('../scrapes/northpro')
 
 test('rimfire').then(f => console.log(f)).catch(e => console.error('ERROR', e.message))
 
