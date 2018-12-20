76184a939671acb43a0759e608626745f72e791c
nexus-uw <nexus-uw@users.noreply.github.com>
2018-12-20 05:40:59 +0000

    use new internal proxy route instead of separate domain (HTTP/2 ftw)

--- src/constants.js
+++ src/constants.js
@@ -38,7 +38,7 @@ const SOURCES = [
   'gothiclinearmoury.ca',
 ]
 
-const PROXY_URL = 'https://images.ammobin.ca'
+const PROXY_URL = 'https://ammobin.ca/images' // 'https://images.ammobin.ca'
 const DATE_FORMAT = 'YYYY-MM-DD'
 const CACHE_REFRESH_HOURS = 4
 
