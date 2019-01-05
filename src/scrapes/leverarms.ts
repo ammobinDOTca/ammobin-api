bd02d858a64a20ff4560058b402959233484d051
nexus-uw <nexus-uw@users.noreply.github.com>
2019-01-05 06:34:23 +0000

    updated leverarms
    - site updated hoster recently

--- src/scrapes/leverarms.ts
+++ src/scrapes/leverarms.ts
@@ -4,27 +4,26 @@ import * as helpers from '../helpers'
 import throat = require('throat')
 
 async function work(type, page = 1) {
-  await helpers.delayScrape('https://www.leverarms.com')
+  await helpers.delayScrape('https://leverarms.com')
 
   console.log(`loading lever arms ${type} ${page}`)
   return axios
-    .get(`https://www.leverarms.com/collections/${type}?page=${page}`)
+    .get(`https://leverarms.com/product-category/${type}/page/${page}`)
     .then(r => {
       let $ = cheerio.load(r.data)
       const items = []
       $('.product').each((index, row) => {
-        const result: any = {}
         const tha = $(row)
 
         if (tha.prop('class').indexOf('outofstock') >= 0) {
           return
         }
 
-        result.link =
-          'https://www.leverarms.com' + tha.find('.title a').prop('href')
-        result.img = 'https:' + tha.find('img').prop('src')
-        result.name = tha.find('.title').text()
-        const priceTxt = tha.find('.money').text()
+        const result: any = {}
+        result.link = tha.find('.woocommerce-LoopProduct-link').prop('href')
+        result.img = tha.find('img').prop('src')
+        result.name = tha.find('.woocommerce-loop-product__title').text()
+        const priceTxt = tha.find('.woocommerce-Price-amount').text()
         result.price = parseFloat(priceTxt.replace('$', ''))
         result.vendor = 'Lever Arms'
         result.province = 'BC'
@@ -32,7 +31,7 @@ async function work(type, page = 1) {
         items.push(result)
       })
 
-      if ($('.next a').length) {
+      if ($('.nav-links .next').length) {
         $ = null // dont hold onto page for recursion
         return work(type, page + 1).then(results => items.concat(results))
       } else {
@@ -45,20 +44,20 @@ export function leverarms(type) {
 
   switch (type) {
     case 'rimfire':
-      return work('rimfire-ammo/Rimfire-Ammo', 1).then(helpers.classifyRimfire)
+      return work('rimfire-ammo', 1).then(helpers.classifyRimfire)
 
     case 'centerfire':
       return Promise.all(
         [
-          'rifle-ammunition/Rifle-Ammunition',
-          'pistol-ammunition/Handgun-Ammo',
+          'rifle-ammunition',
+          // 'pistol-ammunition',
         ].map(t => throttle(() => work(t, 1)))
       )
         .then(helpers.combineResults)
         .then(helpers.classifyCenterfire)
 
     case 'shotgun':
-      return work('shotgun-shells/Shells', 1).then(helpers.classifyShotgun)
+      return work('shotgun-shells', 1).then(helpers.classifyShotgun)
     default:
       return Promise.reject(new Error('unknown type: ' + type))
   }
