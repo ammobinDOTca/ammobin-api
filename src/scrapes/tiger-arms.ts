c29a1c460e9689f1d94a1838d1d3f29a6431e53b
nexus-uw <nexus-uw@users.noreply.github.com>
2019-03-23 05:47:58 +0000

    fixed tiger arms

--- src/scrapes/tiger-arms.ts
+++ src/scrapes/tiger-arms.ts
@@ -1,66 +1,53 @@
-import axios from 'axios'
-import cheerio = require('cheerio')
 import * as helpers from '../helpers'
 import throat = require('throat')
-import { AmmoType, IAmmoListing } from '../graphql-types'
-async function fn(type, page = 1): Promise<IAmmoListing[]> {
-  await helpers.delayScrape('http://www.tigerarms.ca')
+import { Province, AmmoType, IAmmoListing } from '../graphql-types'
 
-  return axios
-    .get(
-      `http://www.tigerarms.ca/product-category/ammunition/${type}/page/${page}/`
-    )
-    .then(r => {
-      let $ = cheerio.load(r.data)
-      const items = []
-      $('.product-inner').each((index, row) => {
-        const result: any = {}
-        const tha = $(row)
-        if (tha.find('.gema75_soldout_badge_new_2327').length) {
-          return
-        }
-
-        result.link = tha.find('.woocommerce-LoopProduct-link').prop('href')
-        result.img = tha.find('.attachment-shop_catalog').prop('src')
-        result.name = tha.find('.product-info h3').text()
-        result.price = parseFloat(
-          tha
-            .find('.amount')
-            .text()
-            .replace('$', '')
-        )
-        result.vendor = 'Tiger Arms'
-        result.province = 'BC'
-        items.push(result)
-      })
-
-      if ($('.fa-angle-right').length) {
-        // load next page
-        $ = null // dont hold onto current page
-        console.log('loading tiger arms page', page + 1)
-        return fn(type, page + 1).then(ff => ff.concat(items))
-      } else {
-        return items
-      }
-    })
-}
+import { scrape, Info, Selectors } from './common'
 
 export function tigerArms(type: AmmoType): Promise<IAmmoListing[]> {
   const throttle = throat(1)
-  switch (type) {
-    case 'rimfire':
-      return fn('rifle-ammo').then(i => helpers.classifyBullets(i, type))
 
-    case 'centerfire':
-      return Promise.all([
-        throttle(() => fn('handgun-ammo')),
-        throttle(() => fn('rifle-ammo')),
-      ])
+  const info: Info = {
+    site: 'tigerarms.ca',
+    vendor: 'Tiger Arms',
+    provinces: [Province.BC],
+  }
+
+  const selectors: Selectors = {
+    item: '.product',
+    name: '.desc h4',
+    img: '.wp-post-image',
+    link: 'a',
+    price: '.amount',
+    outOfStock: '.gema75_soldout_badge_new_2327',
+  }
+  const BASE = 'https://tigerarms.ca/product-category/ammunition'
+
+  switch (type) {
+    case AmmoType.rimfire:
+      return scrape(
+        p => `${BASE}/rimfire-ammo?paged=${p}`,
+        info,
+        selectors
+      ).then(items => helpers.classifyBullets(items, type))
+
+    case AmmoType.centerfire:
+      return Promise.all(
+        ['rifle-ammo', 'handgun-ammo'].map(s =>
+          throttle(() =>
+            scrape(p => `${BASE}/${s}?paged=${p}`, info, selectors)
+          )
+        )
+      )
         .then(helpers.combineResults)
         .then(i => helpers.classifyBullets(i, type))
 
-    case 'shotgun':
-      return fn('shotgun-ammo').then(helpers.classifyShotgun)
+    case AmmoType.shotgun:
+      return scrape(
+        p => `${BASE}/shotgun-ammo?paged=${p}`,
+        info,
+        selectors
+      ).then(items => helpers.classifyBullets(items, type))
     default:
       return Promise.reject(new Error('unknown type: ' + type))
   }
