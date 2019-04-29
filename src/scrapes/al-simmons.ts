288bf47a7cb85573d0f9ad6e811419d159edd646
Simon R <nexus-uw@users.noreply.github.com>
2019-04-28 20:51:15 -0700

    fixed + upgraded al simmons

--- src/scrapes/al-simmons.ts
+++ src/scrapes/al-simmons.ts
@@ -1,77 +1,38 @@
-import axios from 'axios'
-
-import cheerio from 'cheerio'
 import * as helpers from '../helpers'
-import throat from 'throat'
-import { AmmoType, IAmmoListing } from '../graphql-types'
+import { AmmoType, IAmmoListing, Province } from '../graphql-types'
 
-const SITE = 'https://alsimmonsgunshop.com'
-
-async function work(type: string, page = 1): Promise<IAmmoListing[]> {
-  await helpers.delayScrape(SITE)
-  console.log(`loading al simmons ${type} ${page}`)
-
-  return axios
-    .get(
-      `${SITE}/product-category/ammunition/${type}/page/${page}/?instock_products=in`
-    )
-    .then(r => {
-      let $ = cheerio.load(r.data)
-      const items = []
-      $('.type-product').each((index, row) => {
-        const result: any = {}
-        const tha = $(row)
-        result.link = tha.find('.post-image a').prop('href')
-        result.img = 'https:' + tha.find('.wp-post-image').prop('src')
-        result.name = tha.find('.product-content-inner h3').text()
-        const priceTxt = tha
-          .find('.price')
-          .last()
-          .text() // sale price come last...
-        result.price = parseFloat(priceTxt.replace('$', ''))
-
-        result.vendor = 'Al Simmons'
-        result.province = 'ON'
-
-        items.push(result)
-      })
-
-      if (items.length && items.length === 10) {
-        $ = null // dont hold onto page for recursion
-        return work(type, page + 1)
-          .then(results => items.concat(results))
-          .catch(e => {
-            // sometimes go too far and get 404
-            if (e.response && e.response.status === 404) {
-              console.warn('went too far with al simmons page: ' + page)
-              return items
-            }
-            throw e
-          })
-      } else {
-        return items
-      }
-    })
-}
+import { scrape, Info, Selectors } from './common'
 
 export function alSimmons(type: AmmoType): Promise<IAmmoListing[]> {
-  const throttle = throat(1)
+  const info: Info = {
+    site: 'alsimmonsgunshop.com',
+    vendor: `Al Simmons`,
+    provinces: [Province.ON],
+  }
+
+  const selectors: Selectors = {
+    item: '.product ',
+    name: '.product-title',
+    img: '.wp-post-image',
+    link: '.product-title a',
+    price: '.price',
+
+    nextPage: '.next',
+    outOfStock: '.out-of-stock',
+  }
 
   switch (type) {
     case AmmoType.rimfire:
-      return work('rimfire-ammunition').then(helpers.classifyRimfire)
-
     case AmmoType.centerfire:
-      return Promise.all(
-        ['rifle-ammunition', 'handgun-ammunition'].map(t =>
-          throttle(() => work(t, 1))
-        )
-      )
-        .then(helpers.combineResults)
-        .then(helpers.classifyCenterfire)
-
     case AmmoType.shotgun:
-      return Promise.resolve([])
+      return scrape(
+        p =>
+          `https://${
+            info.site
+          }/product-category/ammunition/page/${p}/?product_count=100`,
+        info,
+        selectors
+      ).then(helpers.classify(type))
 
     default:
       return Promise.reject(new Error('unknown type: ' + type))
