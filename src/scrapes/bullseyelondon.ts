dc0f6d8fa51dfa59bf08552f56d11a5d4a58d15a
Simon R <nexus-uw@users.noreply.github.com>
2019-05-11 18:17:14 -0700

    converted bullseyelondon to normal scrape + added reloading

--- src/scrapes/bullseyelondon.ts
+++ src/scrapes/bullseyelondon.ts
@@ -1,27 +1,47 @@
 import * as helpers from '../helpers'
-import { ItemType, IItemListing } from '../graphql-types'
-function makeBullsReq(ammotype) {
-  return helpers.makeWrapApiReq('bullseye', ammotype).then(d =>
-    d.items.map((i: IItemListing) => {
-      i.vendor = 'Bulls Eye London'
-      return i
-    })
-  )
+import { ItemType, IItemListing, Province } from '../graphql-types'
+import { Info, Selectors, scrape } from './common'
+
+function work(path: String): Promise<IItemListing[]> {
+  const info: Info = {
+    site: 'bullseyelondon.com',
+    vendor: `Bulls Eye London`,
+    provinces: [Province.ON],
+  }
+
+  const selectors: Selectors = {
+    item: '.item',
+    name: '.product-name',
+    img: '.product-image img',
+    link: 'a',
+    price: '.regular-price',
+  }
+  const BASE = 'https://www.' + info.site
+  return scrape(p => `${BASE}/${path}/?limit=all`, info, selectors)
 }
 
 export function bullseyelondon(type: ItemType): Promise<IItemListing[]> {
-  if (type === ItemType.rimfire) {
-    return makeBullsReq('rimfire-ammunition').then(helpers.classifyRimfire)
-  } else if (type === ItemType.centerfire) {
-    return Promise.all([
-      makeBullsReq('pistol-ammunition'),
-      makeBullsReq('rifle-ammunition'),
-    ])
-      .then(helpers.combineResults)
-      .then(helpers.classifyCenterfire)
-  } else if (type === ItemType.shotgun) {
-    return makeBullsReq('shotgun').then(helpers.classifyShotgun)
-  } else {
-    throw new Error(`unknown type: ${type}`)
+  switch (type) {
+    case ItemType.rimfire:
+      return work('ammunition/rimfire-ammunition').then(helpers.classifyRimfire)
+    case ItemType.centerfire:
+      return Promise.all([
+        work('ammunition/centerfire-ammunition'),
+        work('ammunition/surplus-ammunition'),
+      ])
+        .then(helpers.combineResults)
+        .then(helpers.classifyCenterfire)
+    case ItemType.shotgun:
+      return work('ammunition/shotgun').then(helpers.classifyShotgun)
+    case ItemType.shot:
+      return work('reloading/bullets')
+    case ItemType.case:
+      return work('reloading/brass')
+    case ItemType.powder:
+      return work('reloading/powder')
+    case ItemType.primer:
+      return work('reloading/primers')
+    default:
+      throw new Error(`unknown type: ${type}`)
   }
 }
