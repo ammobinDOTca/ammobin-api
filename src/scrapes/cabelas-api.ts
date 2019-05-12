5262be0cf5904c54af970221f0fcc9beb59766a8
Simon R <nexus-uw@users.noreply.github.com>
2019-05-11 18:17:48 -0700

    added reloading for cabelas

--- src/scrapes/cabelas-api.ts
+++ src/scrapes/cabelas-api.ts
@@ -2,7 +2,9 @@ import axios from 'axios'
 import cheerio = require('cheerio')
 import throat = require('throat')
 import helpers = require('../helpers')
-import { ItemType, IItemListing } from '../graphql-types'
+import { ItemType, IItemListing, Province } from '../graphql-types'
+import { Info, Selectors, scrape } from './common'
+
 const columns = [
   ['link', 'Article #'],
   ['brand', 'Brand'],
@@ -15,6 +17,29 @@ const columns = [
   ['price', 'Price'],
   ['status', 'Status'],
 ].map(c => c.map(s => s.toLowerCase()))
+
+const info: Info = {
+  site: 'cabelas.ca',
+  vendor: `Cabela's`,
+  provinces: [
+    Province.BC,
+    Province.AB,
+    Province.SK,
+    Province.MB,
+    Province.ON,
+    Province.NB,
+    Province.NS,
+  ],
+}
+
+const selectors: Selectors = {
+  item: '.productCard',
+  name: '.productCard-heading',
+  img: '.productCard-thumb img',
+  link: '.productCard-heading a',
+  price: '.price-primary',
+}
+const BASE = 'https://www.' + info.site
 
 function classify(d) {
   if (!d.titles || !d.items) {
@@ -37,8 +62,8 @@ function classify(d) {
 
   const result = items.map(i => {
     const item: any = {
-      vendor: `Cabela's`,
-      province: 'BC,AB,SK,MB,ON,NB,NS',
+      vendor: info.vendor,
+      provinces: info.provinces,
     }
     for (let index = 0; index < 12; index++) {
       let value = i[index.toString()]
@@ -48,7 +73,7 @@ function classify(d) {
       }
 
       if (prop === 'link') {
-        value = 'https://www.cabelas.ca' + value
+        value = BASE + value
       } else if (prop === 'price') {
         value = parseFloat(value.replace('$', ''))
       } else if (prop === 'count') {
@@ -70,12 +95,10 @@ function classify(d) {
 }
 
 async function makeCabelasCalibre(ammotype, subtype) {
-  await helpers.delayScrape('https://www.cabelas.ca')
+  await helpers.delayScrape(BASE)
 
   return axios
-    .get(
-      `https://www.cabelas.ca/checkproductvariantavailability/${ammotype}?specs=${subtype}`
-    )
+    .get(`${BASE}/checkproductvariantavailability/${ammotype}?specs=${subtype}`)
     .then(r => {
       const $ = cheerio.load(r.data)
 
@@ -115,45 +138,58 @@ function makeCabelasReq(itemType) {
   //    .then(classify);
 }
 
+function work(path: String): Promise<IItemListing[]> {
+  return scrape(p => `${BASE}/category/${path}/?pagesize=100`, info, selectors)
+}
+
 export function cabelas(type: ItemType): Promise<IItemListing[]> {
   const throttle = throat(1)
-  // TODO: need to fix classifier for rimfire+shotgun
-  if (type === 'rimfire') {
-    return makeCabelasReq('936').then(helpers.classifyRimfire)
-  } else if (type === 'shotgun') {
-    return Promise.all([
-      throttle(() => makeCabelasReq('928')),
-      throttle(() => makeCabelasReq('922')),
-      throttle(() => makeCabelasReq('923')),
-      throttle(() => makeCabelasReq('924')),
-      throttle(() => makeCabelasReq('926')),
-    ])
-      .then(helpers.combineResults)
-      .then(helpers.classifyCenterfire)
-  } else if (type === 'centerfire') {
-    return Promise.all([
-      makeCabelasReq('916'),
-      throttle(() => makeCabelasCalibre('933', '19365')), // 204 ruger
-      throttle(() => makeCabelasCalibre('933', '20529')), // 22-250 rem
-      throttle(() => makeCabelasCalibre('933', '20476')), // 223 rem
-      throttle(() => makeCabelasCalibre('933', '20554')), // 243 win
-      throttle(() => makeCabelasCalibre('933', '20543')), // 270 win
-      throttle(() => makeCabelasCalibre('933', '20556')), // 270 wsm
-      throttle(() => makeCabelasCalibre('933', '20641')), // 300 rem mag
-      throttle(() => makeCabelasCalibre('933', '20547')), // 30 30 win
-      throttle(() => makeCabelasCalibre('933', '19413')), // 303 brit
-      throttle(() => makeCabelasCalibre('933', '20486')), // 308 wim
-      throttle(() => makeCabelasCalibre('933', '20483')), // .30-06 Springfield
-      throttle(() => makeCabelasCalibre('933', '20516')), // 338 lapua
-      throttle(() => makeCabelasCalibre('933', '20491')), // 40/70
-      throttle(() => makeCabelasCalibre('933', '20494')), // 7.62x39
-      throttle(() => makeCabelasCalibre('933', '20561')), // 7mm-08
-      throttle(() => makeCabelasCalibre('933', '20552')), // 7mm rem mag
-      throttle(() => makeCabelasCalibre('933', '20557')), // 7mm wm
-    ])
-      .then(helpers.combineResults)
-      .then(helpers.classifyCenterfire) // need to convert their calibres to ours
-  } else {
-    throw new Error(`unknown type received: ${type}`)
+
+  switch (type) {
+    case ItemType.rimfire:
+      return makeCabelasReq('936').then(helpers.classifyRimfire)
+    case ItemType.shotgun:
+      return Promise.all([
+        throttle(() => makeCabelasReq('928')),
+        throttle(() => makeCabelasReq('922')),
+        throttle(() => makeCabelasReq('923')),
+        throttle(() => makeCabelasReq('924')),
+        throttle(() => makeCabelasReq('926')),
+      ])
+        .then(helpers.combineResults)
+        .then(helpers.classifyShotgun)
+    case ItemType.centerfire:
+      return Promise.all([
+        throttle(() => makeCabelasReq('916')),
+        throttle(() => makeCabelasCalibre('933', '19365')), // 204 ruger
+        throttle(() => makeCabelasCalibre('933', '20529')), // 22-250 rem
+        throttle(() => makeCabelasCalibre('933', '20476')), // 223 rem
+        throttle(() => makeCabelasCalibre('933', '20554')), // 243 win
+        throttle(() => makeCabelasCalibre('933', '20543')), // 270 win
+        throttle(() => makeCabelasCalibre('933', '20556')), // 270 wsm
+        throttle(() => makeCabelasCalibre('933', '20641')), // 300 rem mag
+        throttle(() => makeCabelasCalibre('933', '20547')), // 30 30 win
+        throttle(() => makeCabelasCalibre('933', '19413')), // 303 brit
+        throttle(() => makeCabelasCalibre('933', '20486')), // 308 wim
+        throttle(() => makeCabelasCalibre('933', '20483')), // .30-06 Springfield
+        throttle(() => makeCabelasCalibre('933', '20516')), // 338 lapua
+        throttle(() => makeCabelasCalibre('933', '20491')), // 40/70
+        throttle(() => makeCabelasCalibre('933', '20494')), // 7.62x39
+        throttle(() => makeCabelasCalibre('933', '20561')), // 7mm-08
+        throttle(() => makeCabelasCalibre('933', '20552')), // 7mm rem mag
+        throttle(() => makeCabelasCalibre('933', '20557')), // 7mm wm
+      ])
+        .then(helpers.combineResults)
+        .then(helpers.classifyCenterfire) // need to convert their calibres to ours
+    case ItemType.shot:
+      return work('reloading-bullets/2552')
+    case ItemType.case:
+      return work('reloading-brass/2553')
+    case ItemType.powder:
+      return work('reloading-powder/987')
+    case ItemType.primer:
+      return work('reloading-primers/2554')
+    default:
+      throw new Error(`unknown type received: ${type}`)
   }
 }
