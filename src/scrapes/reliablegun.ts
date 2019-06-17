49ee140fff6bc87fdcd3e5cfbeb2bf6c069ff423
nexus-uw <nexus-uw@users.noreply.github.com>
2019-06-17 06:47:34 +0000

    updated reliable to common + reloading

--- src/scrapes/reliablegun.ts
+++ src/scrapes/reliablegun.ts
@@ -1,39 +1,55 @@
 import * as helpers from '../helpers'
+import throat from 'throat'
 
-async function makeReliableRequest(ammotype, page = 1) {
-  await helpers.delayScrape('https://www.reliablegun.com')
+import { Province, IItemListing, ItemType } from '../graphql-types'
+import { scrape, Info, Selectors } from './common'
+const throttle = throat(1)
+export function reliablegun(type: ItemType): Promise<IItemListing[]> {
+  const info: Info = {
+    site: 'reliablegun.com',
+    vendor: `Reliable Gun`,
+    provinces: [Province.BC],
+  }
 
-  return helpers.makeWrapApiReq('reliablegun', ammotype, page).then(d => {
-    console.log(
-      `reliablegun:  loaded ${ammotype} page${d.page} of ${d.lastPage}`
+  const selectors: Selectors = {
+    item: '.item-box',
+    name: '.product-title',
+    img: '.picture img',
+    link: '.picture a',
+    price: '.price',
+    nextPage: '.next-page',
+    //outOfStock: '.out-of-stock',
+  }
+  const work = t =>
+    scrape(
+      p =>
+        `http://www.${info.site}/en/${t}?pageSize=96&viewMode=grid&orderBy=0&pageNumber=${p}`,
+      info,
+      selectors
     )
-    if (!isNaN(d.lastPage) && d.page < d.lastPage) {
-      return makeReliableRequest(ammotype, page + 1).then(dd =>
-        d.items.concat(dd)
-      )
-    } else {
-      return d.items
-    }
-  })
-}
 
-export function reliablegun(type) {
-  if (type === 'rimfire') {
-    return makeReliableRequest('rimfire-ammunition').then(
-      helpers.classifyRimfire
-    )
-  } else if (type === 'centerfire') {
-    return Promise.all([
-      makeReliableRequest('rifle-ammunition'), // multi page
-      makeReliableRequest('hand-gun-ammunition'), // multi page
-    ])
-      .then(helpers.combineResults)
-      .then(helpers.classifyCenterfire)
-  } else if (type === 'shotgun') {
-    return makeReliableRequest('shotgun-ammunition').then(
-      helpers.classifyShotgun
-    )
-  } else {
-    throw new Error(`unknown type: ${type}`)
+  switch (type) {
+    case ItemType.rimfire:
+      return work('rimfire-ammunition')
+
+    case ItemType.centerfire:
+      return Promise.all(
+        ['rifle-ammunition', 'hand-gun-ammunition'].map(f =>
+          throttle(() => work(f))
+        )
+      ).then(helpers.combineResults)
+    case ItemType.shotgun:
+      return work('shotgun-ammunition')
+    case ItemType.primer:
+      return work('primers')
+
+    case ItemType.shot:
+      return work('bullets')
+    case ItemType.powder:
+      return work('gun-powder')
+    case ItemType.case:
+      return work('brass')
+    default:
+      throw new Error(`unknown type: ${type}`)
   }
 }
