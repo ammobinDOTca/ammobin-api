877ce2b62b66d8625920221d5f610a20823aa534
nexus-uw <nexus-uw@users.noreply.github.com>
2019-06-22 05:44:58 +0000

    siwash

--- src/scrapes/siwash.ts
+++ src/scrapes/siwash.ts
@@ -1,64 +1,51 @@
-import axios from 'axios'
-
-import cheerio from 'cheerio'
-import * as helpers from '../helpers'
 import throat from 'throat'
+import * as helpers from '../helpers'
 import { ItemType, IItemListing, Province } from '../graphql-types'
-
-const SITE = 'https://www.siwashsports.ca'
-
-async function work(type: string, page = 1): Promise<IItemListing[]> {
-  await helpers.delayScrape(SITE)
-  console.log(`loading al siwash sports ${type} ${page}`)
-
-  return axios.get(`${SITE}/product-category/${type}?page=${page}`).then(r => {
-    let $ = cheerio.load(r.data)
-    const items = []
-    $('.product').each((index, row) => {
-      const result: any = {}
-      const tha = $(row)
-
-      result.link = tha.find('a').prop('href')
-      result.img = tha.find('.wp-post-image').prop('src')
-      result.name = tha
-        .find('.woocommerce-loop-product__title')
-        .text()
-        .trim()
-      const priceTxt = tha
-        .find('.amount')
-        .last()
-        .text() // sale price come last...
-      result.price = parseFloat(priceTxt.replace('$', ''))
-
-      result.vendor = 'Siwash Sports'
-      result.provinces = [Province.BC]
-
-      items.push(result)
-    })
-
-    return items
-  })
-}
+import { scrape, Info, Selectors } from './common'
 
 export function siwashSports(type: ItemType): Promise<IItemListing[]> {
   const throttle = throat(1)
+  const info: Info = {
+    site: 'siwashsports.ca',
+    vendor: `Siwash Sports`,
+    provinces: [Province.BC],
+  }
+
+  const selectors: Selectors = {
+    item: '.product ',
+    name: '.woocommerce-loop-product__title',
+    img: '.wp-post-image',
+    link: 'a',
+    price: '.ammount',
+    nextPage: '.next',
+    outOfStock: '.out-of-stock',
+  }
+
+  const work = t =>
+    scrape(
+      p => `https://www.${info.site}/product-category/${t}?page=${p}`,
+      info,
+      selectors
+    )
 
   switch (type) {
     case ItemType.rimfire:
-      return work('ammunition/rimfire-ammunition').then(helpers.classifyRimfire)
+      return work('ammunition/rimfire-ammunition')
 
     case ItemType.centerfire:
       return Promise.all(
         ['ammunition/centerfire-ammunition', 'surplus/surplus-ammunition'].map(
-          t => throttle(() => work(t, 1))
+          t => throttle(() => work(t))
         )
-      )
-        .then(helpers.combineResults)
-        .then(helpers.classifyCenterfire)
+      ).then(helpers.combineResults)
 
     case ItemType.shotgun:
-      return work('ammunition/shotgun-ammunition').then(helpers.classifyShotgun)
-
+      return work('ammunition/shotgun-ammunition')
+    case ItemType.case:
+    case ItemType.shot:
+    case ItemType.primer:
+    case ItemType.powder:
+      return Promise.resolve([])
     default:
       return Promise.reject(new Error('unknown type: ' + type))
   }
