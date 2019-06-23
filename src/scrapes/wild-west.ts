b34e2e09f79df353e7ad996767e162b024f74422
nexus-uw <nexus-uw@users.noreply.github.com>
2019-06-23 22:23:29 +0000

    wildwest reloading

--- src/scrapes/wild-west.ts
+++ src/scrapes/wild-west.ts
@@ -1,64 +1,56 @@
-// https://gun-shop.ca/product-category/ammunition/bulk-ammo/?orderby=price&count=100
-import axios from 'axios'
+import throat from 'throat'
 
-import cheerio = require('cheerio')
 import * as helpers from '../helpers'
+import { ItemType, IItemListing, Province } from '../graphql-types'
+import { scrape, Info, Selectors } from './common'
+const throttle = throat(1)
 
-function fn(type, page = 1) {
-  return axios
-    .get(
-      `https://gun-shop.ca/product-category/ammunition/${type}/page/${page}/?orderby=price-desc`
+export function wildWest(type: ItemType): Promise<IItemListing[]> {
+  const info: Info = {
+    provinces: [Province.AB],
+    site: 'gun-shop.ca',
+    vendor: 'Wild West',
+  }
+
+  const selectors: Selectors = {
+    img: '.attachment-woocommerce_thumbnail',
+    item: '.product',
+    link: 'a',
+    name: '.product-title',
+    price: '.amount',
+    outOfStock: '.out-of-stock-label',
+    nextPage: '.woocommerce-pagination .next',
+  }
+
+  const fn = (t: string) =>
+    scrape(
+      p =>
+        `https://${info.site}/product-category/${t}/page/${p}/?orderby=price-desc`,
+      info,
+      selectors
     )
-    .then(r => {
-      let $ = cheerio.load(r.data)
-      const items = []
-      $('.product').each((index, row) => {
-        const result: any = {}
-        const tha = $(row)
 
-        if (tha.find('.out-of-stock-label').length > 0) {
-          return
-        }
-
-        result.link = tha.find('a').prop('href')
-        result.img = 'https:' + tha.find('.wp-post-image').prop('src')
-        result.name = tha.find('.product-title').text()
-
-        // on sale price comes last
-        result.price = parseFloat(
-          tha
-            .find('.amount')
-            .last()
-            .text()
-            .replace('$', '')
-        )
-
-        result.vendor = 'Wild West'
-        result.province = 'AB'
-        items.push(result)
-      })
-
-      const paginationContainer = $('.woocommerce-pagination')
-      const TODO = false // 20180517: dont want to figure this out tonight
-      if (paginationContainer.length > 0 && TODO) {
-        $ = null
-        console.log(`loading wild west page ${page + 1}`)
-        return fn(type, page + 1).then(f => items.concat(f))
-      } else {
-        return items
-      }
-    })
-}
-
-export function wildWest(type) {
   switch (type) {
-    case 'rimfire':
-    case 'centerfire':
-      return Promise.all([fn('bulk-ammo'), fn('box-ammo')])
-        .then(results => results.reduce((final, r) => final.concat(r), []))
-        .then(i => helpers.classifyBullets(i, type))
-    case 'shotgun':
-      return fn('shotgun-ammo').then(helpers.classifyShotgun)
+    case ItemType.rimfire:
+    case ItemType.centerfire:
+      return Promise.all(
+        ['bulk-ammo', 'box-ammo'].map(t =>
+          throttle(() => fn('ammunition/' + t))
+        )
+      ).then(helpers.combineResults)
+
+    case ItemType.shotgun:
+      return fn('ammunition/shotgun-ammo')
+
+    case ItemType.shot:
+      return fn('reloading-components/projectiles')
+    case ItemType.powder:
+      return fn('reloading-components/powder')
+
+    case ItemType.case:
+    case ItemType.primer:
+      return Promise.resolve(null)
+
     default:
       return Promise.reject(new Error('unknown type: ' + type))
   }
