ac065030176d8398159e6b79308c1da1d0f27844
nexus-uw <nexus-uw@users.noreply.github.com>
2019-06-23 22:00:15 +0000

    reloading for wolverine

--- src/scrapes/wolverine-api.ts
+++ src/scrapes/wolverine-api.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 import throat from 'throat'
 import * as helpers from '../helpers'
+import { ItemType, Province } from '../graphql-types'
 
 /**
  * makeWolverine api req
@@ -23,6 +24,9 @@ function makeWolverine(type) {
     case 'shotgun':
       categoryCode = '300300'
       break
+    case 'reloading':
+      categoryCode = '300100'
+      break
     default:
       return Promise.reject(new Error('unexpected ammo type: ' + type))
   }
@@ -38,6 +42,16 @@ function makeWolverine(type) {
     UsedProductPage: false,
   }
 
+  function isReloadingComponent(item): boolean {
+    return (
+      item.Attributes.filter(
+        a =>
+          a.AttributeName === 'SUBCATEGORY' &&
+          a.AttributeValue === 'RELOADING COMPONENTS'
+      ).length > 0
+    )
+  }
+
   return axios
     .get(
       'https://www.wolverinesupplies.com/WebServices/ProductSearchService.asmx/GetJSONItems?data=' +
@@ -45,59 +59,71 @@ function makeWolverine(type) {
     )
     .then(d => {
       const list = d.data
-      return list.slice(1, list.length - 1).map(i => {
-        let calibre = null
-        let brand = null
+      return list
+        .slice(1, list.length - 1)
+        .filter(
+          i =>
+            i.StockStatus === 'In Stock' &&
+            (type !== 'reloading' || isReloadingComponent(i))
+        )
+        .map(i => {
+          let calibre = null
+          let brand = null
 
-        i.Attributes.forEach(f => {
-          if (f.AttributeName === 'MANUFACTURER') {
-            brand = f.AttributeValue
-          } else if (f.AttributeName === 'CALIBER') {
-            calibre = f.AttributeValue
+          i.Attributes.forEach(f => {
+            if (f.AttributeName === 'MANUFACTURER') {
+              brand = f.AttributeValue
+            } else if (
+              f.AttributeName === 'CALIBER' &&
+              f.AttributeValue !== 'NA'
+            ) {
+              calibre = f.AttributeValue
+            }
+          })
+          const rep = new RegExp(/[^a-zA-Z0-9 ]/, 'g')
+
+          return {
+            name: i.Title.trim(),
+            img:
+              i.ImageFile && i.ImageFile !== 'ImageNotFound'
+                ? 'https://www.wolverinesupplies.com/images/items/Thumbnail/' +
+                  i.ImageFile +
+                  i.ImageExtension
+                : null,
+            vendor: 'Wolverine Supplies',
+            province: Province.MB,
+            price: i.Price,
+            calibre,
+            brand,
+            link:
+              'https://www.wolverinesupplies.com/ProductDetail/' +
+              `${i.ItemNumber}_${i.Title.replace(rep, '-')
+                .split(' ')
+                .join('-')}`,
           }
         })
-        const rep = new RegExp(/[^a-zA-Z0-9 ]/, 'g')
-
-        return {
-          name: i.Title,
-          img:
-            i.ImageFile !== 'ImageNotFound'
-              ? 'https://www.wolverinesupplies.com/images/items/Thumbnail/' +
-                i.ImageFile +
-                i.ImageExtension
-              : null,
-          vendor: 'Wolverine Supplies',
-          province: 'MB',
-          price: i.Price,
-          calibre,
-          brand,
-          link:
-            'https://www.wolverinesupplies.com/ProductDetail/' +
-            `${i.ItemNumber}_${i.Title.replace(rep, '-')
-              .split(' ')
-              .join('-')}`,
-        }
-      })
     })
 }
 
-export function wolverinesupplies(type) {
+export function wolverinesupplies(type: ItemType) {
   const throttle = throat(1)
 
   switch (type) {
-    case 'rimfire':
-      return makeWolverine('rimfire').then(helpers.classifyRimfire)
+    case ItemType.rimfire:
+      return makeWolverine('rimfire')
 
-    case 'centerfire':
+    case ItemType.centerfire:
       return Promise.all(
         ['rifle', 'pistol'].map(t => throttle(() => makeWolverine(t)))
-      )
-        .then(helpers.combineResults)
-        .then(helpers.classifyCenterfire)
+      ).then(helpers.combineResults)
 
-    case 'shotgun':
-      return makeWolverine('shotgun').then(helpers.classifyShotgun)
-
+    case ItemType.shotgun:
+      return makeWolverine('shotgun')
+    case ItemType.case:
+    case ItemType.powder:
+    case ItemType.primer:
+    case ItemType.shot:
+      return makeWolverine('reloading')
     default:
       throw new Error(`unknown type: ${type}`)
   }
