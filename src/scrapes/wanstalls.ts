95999c31f72b551f32570b8b08d4fee77adc97ca
nexus-uw <nexus-uw@users.noreply.github.com>
2019-07-06 18:30:15 +0000

    marked wanstalls as no reloading + updated scrape

--- src/scrapes/wanstalls.ts
+++ src/scrapes/wanstalls.ts
@@ -1,63 +1,48 @@
-import axios from 'axios'
-
-import cheerio = require('cheerio')
 import * as helpers from '../helpers'
 import throat from 'throat'
-const SITE = 'https://www.wanstallsonline.com'
 
-async function getStuff(type) {
-  await helpers.delayScrape(SITE)
-
-  const r = await axios.get(`${SITE}/ammunition/${type}.html?limit=all`)
-  const $ = cheerio.load(r.data)
-  const items = []
-  $('.product-box-1').each((index, row) => {
-    const result: any = {}
-    const tha = $(row)
-
-    result.link = tha.find('.product-image').prop('href')
-    result.img = tha.find('.product-image img').prop('src')
-
-    result.name = tha.find('.product-name').text()
-
-    const salePrice = parseFloat(
-      tha
-        .find('.special-price .price')
-        .text()
-        .replace('$', '')
-    )
-    const regPrice = parseFloat(
-      tha
-        .find('.price-box .price')
-        .text()
-        .replace('$', '')
-    )
-    result.price = isFinite(salePrice) && salePrice > 0 ? salePrice : regPrice
-
-    result.vendor = 'Wanstalls'
-    result.province = 'BC'
-    items.push(result)
-  })
-
-  return items
-}
-
+import { ItemType, IItemListing, Province } from '../graphql-types'
+import { scrape, Info, Selectors } from './common'
 const throttle = throat(1)
 
-export function wanstalls(type) {
+export function wanstalls(type: ItemType): Promise<IItemListing[]> {
+  const info: Info = {
+    site: 'wanstallsonline.com',
+    vendor: 'Wanstalls',
+    provinces: [Province.BC],
+  }
+
+  const selectors: Selectors = {
+    item: '.product-box-1',
+    link: '.product-image',
+    name: '.product-name',
+    img: '.product-image img',
+    price: '.special-price .price , .price-box .price',
+  }
+
+  const getStuff = t =>
+    scrape(
+      p => `https://www.${info.site}/ammunition/${t}.html?limit=all`,
+      info,
+      selectors
+    )
+
   switch (type) {
-    case 'rimfire':
-      return getStuff('rimfire').then(helpers.classifyRimfire)
+    case ItemType.rimfire:
+      return getStuff('rimfire')
 
-    case 'shotgun':
-      return getStuff('shot-gun').then(helpers.classifyShotgun)
+    case ItemType.shotgun:
+      return getStuff('shot-gun')
 
-    case 'centerfire':
+    case ItemType.centerfire:
       return Promise.all(
         ['pistol', 'rifle', 'surplus'].map(t => throttle(() => getStuff(t)))
-      )
-        .then(helpers.combineResults)
-        .then(helpers.classifyCenterfire)
+      ).then(helpers.combineResults)
+    case ItemType.case:
+    case ItemType.powder:
+    case ItemType.shot:
+    case ItemType.primer:
+      return Promise.resolve(null)
     default:
       return Promise.reject(new Error('unknown type: ' + type))
   }
