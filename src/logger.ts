7ba41ca8dbc3ea22512692ff3d187ecfb59c6ec9
nexus-uw <nexus-uw@users.noreply.github.com>
2019-11-10 21:58:04 +0000

    cleaned up logger
    - upgraded to proper typescript

--- src/logger.ts
+++ src/logger.ts
@@ -1,29 +1,27 @@
-let winston = require('winston')
-let config = {
-  host: process.env.FLUNET || 'fluent',
-  port: 24224,
-  timeout: 3.0,
-  requireAckResponse: true, // Add this option to wait response from Fluentd certainly
-}
-let fluentTransport = require('fluent-logger').support.winstonTransport()
+import winston from 'winston'
 
-function createLogger(tag) {
+function createLogger(tag: string) {
   const transports = []
-  // TODO: re-enable this check if not using fluent with aws
-  //if (process.env.FLUENT) {
-  transports.push(new fluentTransport(tag, config))
-  //}
+  if (process.env.FLUENT) {
+    let config = {
+      host: 'fluent',
+      port: 24224,
+      timeout: 3.0,
+      requireAckResponse: true, // Add this option to wait response from Fluentd certainly
+    }
+    let fluentTransport = require('fluent-logger').support.winstonTransport()
+    transports.push(new fluentTransport(tag, config))
+  }
   if (!process.env.DONT_LOG_CONSOLE) {
     transports.push(new winston.transports.Console())
+  }
+
+  if (transports.length === 0) {
+    console.warn('no longer transports configured....')
   }
   return winston.createLogger({
     transports,
   })
-}
-
-module.exports = {
-  apiLogger: createLogger('ammobin.api'),
-  workerLogger: createLogger('ammobin.api'),
 }
 
 export const apiLogger = createLogger('ammobin.api')
