cc0607410632504091254b744a54be111c18c0f6
nexus-uw <nexus-uw@users.noreply.github.com>
2019-11-24 05:04:41 +0000

    added timing metrics to lambda graphql

--- src/api/graphql-lambda.ts
+++ src/api/graphql-lambda.ts
@@ -2,13 +2,18 @@
  * basic lambda to expose graphql as own endpoint
  * without the other cruff
  */
-import { Callback, Context, APIGatewayEvent } from 'aws-lambda'
+import {
+  Callback,
+  Context,
+  APIGatewayEvent,
+  APIGatewayProxyResult,
+} from 'aws-lambda'
 import { ApolloServer } from 'apollo-server-lambda'
 import { getDyanmoItems } from './dynamo-getter'
 import { getScrapeResponses, getItemsFlatListings } from './shared'
 import { typeDefs, vendors, bestPrices } from './graphql'
 import { apiLogger } from '../logger'
-
+import crypto from 'crypto'
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
@@ -20,9 +25,26 @@ const resolvers = {
   },
 }
 
+const secret = process.env.HASH_SECRET || Math.random().toString()
+
 const server = new ApolloServer({ typeDefs, resolvers })
 
 exports.handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
+  const requestId = event.requestContext.requestId
+
+  const ip =
+    event.headers.headers['x-forwarded-for'] ||
+    event.headers.headers['x-real-ip'] ||
+    event.requestContext.identity.sourceIp
+  const sessionId = ip
+    ? crypto
+        .createHmac('sha256', secret)
+        .update(ip)
+        .digest('hex')
+    : 'unknown_ip'
+  delete event.headers.headers['x-forwarded-for']
+  delete event.headers.headers['x-real-ip']
+  const startTime = new Date().getTime()
   let query, variables
   try {
     const r = JSON.parse(event.body)
@@ -35,7 +57,22 @@ exports.handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
     type: 'graphql-query',
     query,
     variables,
+    requestId,
   })
 
-  server.createHandler({ cors: { origin: '*' } })(event, context, cb)
+  server.createHandler({
+    // apigateway handles cors for us
+  })(event, context, (err, result: APIGatewayProxyResult) => {
+    apiLogger.info({
+      type: 'api-req',
+      statusCode: result.statusCode,
+      path: event.path,
+      timeMs: new Date().getTime() - startTime,
+      method: event.httpMethod,
+      headers: event.headers,
+      requestId,
+      sessionId,
+    })
+    cb(err, result)
+  })
 }
