c30d9999e36b7c81932cdc3878957425ff3b9a5c
nexus-uw <nexus-uw@users.noreply.github.com>
2020-10-11 22:12:51 +0000

    fixed barton centerfire
    - todo: other stuff

--- src/scrapes/barton.ts
+++ src/scrapes/barton.ts
@@ -11,17 +11,18 @@ function work(path: String) {
   }
 
   const selectors: Selectors = {
-    item: '.product-item-info',
-    name: '.product-item-link',
-    img: '.product-image-photo',
-    link: '.product-item-link',
+    item: '.product',
+    name: '.title',
+    img: 'img',
+    link: 'a',
     price: '.price',
 
     // pagination not working as expected...
     // nextPage: '.pages-item-next',
   }
+  //https://www.bartonsbigcountry.ca/ammunition/centerfire-rifle/
   const BASE = 'https://' + info.link
-  return scrape(p => `${BASE}/index.php/${path}.html?p=${p}&product_list_limit=30`, info, selectors)
+  return scrape((p) => `${BASE}/${path}`, info, selectors)
 }
 
 export function barton(type: ItemType): Promise<IItemListing[]> {
@@ -32,9 +33,108 @@ export function barton(type: ItemType): Promise<IItemListing[]> {
       return work('ammunition/rimfire')
 
     case ItemType.centerfire:
-      return Promise.all(
-        ['centerfire-pistol', 'centerfire-rifle', 'bulk-surplus'].map(t => throttle(() => work('ammunition/' + t)))
-      ).then(helpers.combineResults)
+      return Promise.all([
+        ...[
+          '17-hornet',
+          // '17-remington-fireball',
+          '204-ruger',
+          '22-hornet',
+          '220-swift',
+          '22-250-remington',
+          '222-remington',
+          '223-remington',
+          '556x45mm-nato',
+          '223-wssm',
+          '224-valkyrie',
+          '243-winchester',
+          '250-savage',
+          '25-06-remington',
+          '257-weatherby-mag',
+          '26-nosler',
+          '260-remington',
+          '264-win',
+          '270-weatherby-mag',
+          '270-win',
+          '270-wsm',
+          '28-nosler',
+          '280-remington',
+          '280-ackley-improved',
+          '284-win',
+          '30-carbine',
+          '30-nosler',
+          '6mm-remington',
+          '6mm-creedmoor',
+          '65-creedmoor',
+          '65-prc',
+          '65x55-swedish',
+          '65x300-weatherby-mag',
+          '7mm-08-remington',
+          '7mm-remington-mag',
+          '7mm-rum',
+          '7mm-stw',
+          '7mm-weatherby-mag',
+          '7mm-wsm',
+          '762x39mm',
+          '762x54mmr',
+          '8mm-remington-mag',
+          '30-06-springfield',
+          '30-378-weatherby-mag',
+          '30-30-winchester',
+          '300-aac-blackout',
+          '300-rcm',
+          '300-wsm',
+          '300-weatherby-mag',
+          '300-win',
+          '300-prc',
+          '300-rum',
+          '300-norma-mag',
+          '303-british',
+          '308-marlin-express',
+          '308-win',
+          '32-winchester-special',
+          '325-wsm',
+          '338-federal',
+          '338-lapua',
+          '338-rum',
+          '338-winchester-magnum',
+          '338-378-weatherby-magnum',
+          '35-whelen',
+          '375-h-h-magnum',
+          '376-steyr',
+          '416-rigby',
+          '416-ruger',
+          '44-mag',
+          '44-40-win',
+          '45-70-government',
+          '450-bushmaster',
+          '454-casull',
+          '458-winchester-magnum',
+          '50-bmg',
+        ].map((t) => throttle(() => work('ammunition/centerfire-rifle/' + t))),
+
+        ...[
+          '762x25mm-tokarev',
+          '9mm',
+          '32-auto',
+          '38-special',
+          '10mm',
+          '357-mag',
+          '380-auto',
+          '40-s-w',
+          '41-rem',
+          '44-40-win',
+          '44-special',
+          '44-mag',
+          '45-acp',
+          '45-colt',
+          '454-casull',
+          '460-s-w',
+          '475-linebaugh',
+          '480-ruger',
+          '500-s-w',
+          '50-ae',
+        ].map((t) => throttle(() => work('ammunition/centerfire-pistol/' + t))),
+      ]).then(helpers.combineResults)
     case ItemType.shotgun:
       return work('ammunition/shotgun-ammunition')
 
