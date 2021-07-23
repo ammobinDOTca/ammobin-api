6dd1a254acc6ba79e489c85b218c669245cc68d5
Simon R <nexus-uw@users.noreply.github.com>
2021-07-23 10:22:44 -0700

    dont run nuxt beta tests
    bucket is empty

--- test/nuxt/index.ts
+++ test/nuxt/index.ts
@@ -8,7 +8,10 @@ function checkIndex(base) {
     .then((r) => assert(r.status === 200, `expected http 200 for GET ${url}, got ${r.status}`))
 }
 
-export async function handler({ base }) {
+export async function handler({ base }: { base: string }) {
+  if (base.startsWith('https://beta.')) {
+    return // dont worry about beta stage, since bucket is usually empty to save on costs
+  }
   console.log('---nuxt---')
   await checkIndex(base)
   console.log('  index :+1:')
