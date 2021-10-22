9a0c09b4b335d15a7705e5b96936f03851aa83a1
Simon R <nexus-uw@users.noreply.github.com>
2021-10-22 16:42:33 -0700

    added more nuxt checks
    - now using workers, so want to test out those pages more

--- test/nuxt/index.ts
+++ test/nuxt/index.ts
@@ -8,11 +8,30 @@ function checkIndex(base) {
     .then((r) => assert(r.status === 200, `expected http 200 for GET ${url}, got ${r.status}`))
 }
 
+function checkItemPage(base) {
+  const url = base + '/en/centerfire/9MM' + `?test_id=${Math.random()}`
+  return axios
+    .get(url, { headers: { 'User-Agent': USER_AGENT } })
+    .then((r) => assert(r.status === 200, `expected http 200 for GET ${url}, got ${r.status}`))
+}
+
+function checkFrench(base) {
+  const url = base + '/fr/rimfire/22LR' + `?test_id=${Math.random()}`
+  return axios
+    .get(url, { headers: { 'User-Agent': USER_AGENT } })
+    .then((r) => assert(r.status === 200, `expected http 200 for GET ${url}, got ${r.status}`))
+}
+
 export async function handler({ base }: { base: string }) {
-  if (base.startsWith('https://beta.')) {
-    return // dont worry about beta stage, since bucket is usually empty to save on costs
-  }
   console.log('---nuxt---')
   await checkIndex(base)
   console.log('  index :+1:')
+
+  await checkItemPage(base)
+  console.log('  checkItemPage :+1:')
+
+  if (base.includes('.ca')) {
+    await checkFrench(base)
+    console.log('  checkFrench :+1:')
+  }
 }
