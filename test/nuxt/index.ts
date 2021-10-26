9552ac6f1f46b126fe1399307ca58d7f8f2eef09
Simon R <nexus-uw@users.noreply.github.com>
2021-10-26 16:19:12 -0700

    worker mapping is not handling 404 very well, change integ test to provide locale

--- test/nuxt/index.ts
+++ test/nuxt/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { assert, USER_AGENT } from '../common'
 
 function checkIndex(base) {
-  const url = base + '/' + `?test_id=${Math.random()}`
+  const url = base + '/en/' + `?test_id=${Math.random()}`
   return axios
     .get(url, { headers: { 'User-Agent': USER_AGENT } })
     .then((r) => assert(r.status === 200, `expected http 200 for GET ${url}, got ${r.status}`))
